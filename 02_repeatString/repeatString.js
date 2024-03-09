ed511decda3d3d0db27dff88bc7b546cb04d4387
Jannik Hodes <jannikhodes@gmx.de>
2024-03-09 17:24:01 +0100

    getting comfy with npm tests while working through exercise 1 and 2  of js exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if( num < 0){
+        return "ERROR";
+    }
+    let outStr='';
+    for (let i=0;i<num;i++){
+        outStr = outStr + str;
+        console.log(outStr);
+    }
+    return outStr;
 };
 
 // Do not edit below this line
